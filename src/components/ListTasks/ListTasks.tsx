39ada6e67244fc6e71fcddfb521d2f628fa59f7e
nikitsch <nikitsch5@gmail.com>
2023-07-26 19:31:53 +0300

    feat: filter tags

--- src/components/ListTasks/ListTasks.tsx
+++ src/components/ListTasks/ListTasks.tsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import { useAppSelector } from "../../store/hooks/hook";
 import { TaskItem } from "./TaskItem/TaskItem";
-
 
 export const TaskList: React.FC = () => {
 
   const tasks = useAppSelector(state => state.tasks.list);
 
-  return (
+  const [filterHash, setFilter] = useState("all")
 
-    <ul>
-      {tasks.map((item) => {
-        return <TaskItem key={item.id} {...item} />
-      })}
-    </ul>
+  const setToFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault()
+    setFilter(event.target.value)
+  }
+
+  const lists = tasks?.map(item => ["all"].concat(item.hashtag)).flat()
+  const exercicesId = lists.filter((value, index, array) => array.indexOf(value) === index);
+
+  return (
+    <>
+      <div>
+        <select onChange={setToFilter}>
+          {exercicesId.map((list, index) => <option key={index} value={list}>{list}</option>)}
+        </select>
+      </div>
+      <ul>
+        {tasks.map((item) => {
+          if (filterHash === "all") return <TaskItem key={item.id} {...item} />
+          if (item.hashtag.includes(filterHash)) {
+            return (
+              <TaskItem key={item.id} {...item} />
+            )
+          }
+        })}
+      </ul>
+    </>
   );
 };
